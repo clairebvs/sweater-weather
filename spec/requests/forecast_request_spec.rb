55eb4fed6a5fc0dfa723c8f4f303fe7be25b5eeb
ClaireBvs <clairembeauvais@gmail.com>
2018-11-04 07:40:56 -0700

    Add request test GET api/v1/forecast [#1]

---
+++ spec/requests/forecast_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "GET /api/v1/forecast?location=denver,co" do
+  it "returns a JSON response with weather forecast for the location" do
+
+    get "/api/v1/forecast?location=denver,co"
+
+    expect(response.status).to eq 200
+
+    weather = JSON.parse(response.body, symbolize_names: true)
+
+    expect(weather).to have_key(:forecast)
+    expect(weather[:forecast]).to have_key(:current_weather)
+    expect(weather[:forecast]).to have_key(:hourly_weather)
+    expect(weather[:forecast]).to have_key(:day_weather)
+  end
+end
