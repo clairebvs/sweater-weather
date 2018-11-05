fd0e0e9e044d10bb5b336a74eaecd43646456437
ClaireBvs <clairembeauvais@gmail.com>
2018-11-05 16:27:19 -0700

    Add private method #user_params and create user for UserController [#7]

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,4 +1,13 @@
 class Api::V1::UserController < ApplicationController
   def create
+    user = User.new(user_params)
+    saved_user = user.save
+    render json: saved_user, status: 201
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
