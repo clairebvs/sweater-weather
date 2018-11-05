60837c5476037b0ffe125a560c77c8d98015ebce
ClaireBvs <clairembeauvais@gmail.com>
2018-11-05 11:04:45 -0700

    render correct JSON for backgrounds endpoint [#1]

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::BackgroundsController < ApplicationController
   def show
     background_image = BackgroundImageFacade.new(params[:location])
 
-    render json: background_image
+    render json: BackgroundImageFacadeSerializer.new(background_image)
   end
 end
