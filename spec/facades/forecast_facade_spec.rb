adfc1f2c0516362d04d600a9c206b4601a1bb7a8
ClaireBvs <clairembeauvais@gmail.com>
2018-11-04 19:24:06 -0700

    Refactor test for #hourly_weather [#4]

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -23,13 +23,35 @@ describe ForecastFacade do
 
     context '#current_weather' do
       it 'returns all weather forecast data at the current time' do
-        expect(subject.current_weather).to be_a CurrentWeather
+        expect(subject.current_weather).to be_a Hash
+        expect(subject.current_weather).to have_key(:apparentTemperature)
+        expect(subject.current_weather).to have_key(:cloudCover)
+        expect(subject.current_weather).to have_key(:humidity)
+        expect(subject.current_weather).to have_key(:icon)
+        expect(subject.current_weather).to have_key(:time)
+        expect(subject.current_weather).to have_key(:summary)
+        expect(subject.current_weather).to have_key(:precipProbability)
+        expect(subject.current_weather).to have_key(:temperature)
+        expect(subject.current_weather).to have_key(:windSpeed)
+        expect(subject.current_weather).to have_key(:uvIndex)
+        expect(subject.current_weather).to have_key(:visibility)
       end
     end
 
     context '#hourly_weather' do
       it 'returns all weather forecast data every hour' do
-        expect(subject.hourly_weather).to be_a HourlyWeather
+        expect(subject.hourly_weather).to be_an Array
+        expect(subject.hourly_weather.first).to have_key(:apparentTemperature)
+        expect(subject.hourly_weather.first).to have_key(:cloudCover)
+        expect(subject.hourly_weather.first).to have_key(:humidity)
+        expect(subject.hourly_weather.first).to have_key(:icon)
+        expect(subject.hourly_weather.first).to have_key(:time)
+        expect(subject.hourly_weather.first).to have_key(:summary)
+        expect(subject.hourly_weather.first).to have_key(:precipProbability)
+        expect(subject.hourly_weather.first).to have_key(:temperature)
+        expect(subject.hourly_weather.first).to have_key(:windSpeed)
+        expect(subject.hourly_weather.first).to have_key(:uvIndex)
+        expect(subject.hourly_weather.first).to have_key(:visibility)
       end
     end
 
