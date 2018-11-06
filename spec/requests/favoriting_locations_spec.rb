d12968d36bd328d02de05001f84467bf6da26700
ClaireBvs <clairembeauvais@gmail.com>
2018-11-06 12:35:22 -0700

    add explicit key for request test api/v1/favorites [#9]

--- spec/requests/favoriting_locations_spec.rb
+++ spec/requests/favoriting_locations_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 describe 'POST /api/v1/favorites' do
   it 'returns a JSON response ' do
-    user = create(:user)
+    user = User.create(password: "password", email: "myemail@so.com", api_key: "jgn983hy48thw9begh98h4539h4")
+
     favorite_params = { api_key: "jgn983hy48thw9begh98h4539h4",
                         location: "Denver, CO"
-                  }
+                      }
 
     post "/api/v1/favorites", params: favorite_params
 
@@ -15,16 +16,6 @@ describe 'POST /api/v1/favorites' do
     expect(favorite[:data]).to have_key(:id)
     expect(favorite[:data]).to have_key(:type)
     expect(favorite[:data]).to have_key(:attributes)
-    expect(favorite[:data][:attributes]).to have_key(:api_key)
     expect(favorite[:data][:attributes]).to have_key(:location)
   end
 end
-
-# add fail if no api_key
-# add fail if no location
-
-# body:
-# {
-#   "location": "Denver, CO", # If you decide to store cities in your database you can send an id if you prefer
-#   "api_key": "jgn983hy48thw9begh98h4539h4"
-# }
