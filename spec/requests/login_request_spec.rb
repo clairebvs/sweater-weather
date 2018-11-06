bd163b6b2d5c2a7cc35336d782c98a81b77112d0
ClaireBvs <clairembeauvais@gmail.com>
2018-11-06 08:22:54 -0700

    Add expect to test correct api key for login request [#8]

--- spec/requests/login_request_spec.rb
+++ spec/requests/login_request_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe 'POST /api/v1/sessions' do
   it 'returns a JSON response with an api_key' do
+    user = create(:user)
+
     user_params = {
                     email: "whatever@example.com",
                     password: "password",
@@ -11,12 +13,14 @@ describe 'POST /api/v1/sessions' do
 
     expect(response.status).to eq 200
 
-    account = JSON.parse(response.body, symbolize_names: true)
+    login = JSON.parse(response.body, symbolize_names: true)
 
-    expect(account).to have_key(:data)
-    expect(account[:data]).to have_key(:id)
-    expect(account[:data]).to have_key(:type)
-    expect(account[:data]).to have_key(:attributes)
-    expect(account[:data][:attributes]).to have_key(:api_key)
+    expect(login[:data][:attributes][:api_key]).to eq(user.api_key)
+
+    expect(login).to have_key(:data)
+    expect(login[:data]).to have_key(:id)
+    expect(login[:data]).to have_key(:type)
+    expect(login[:data]).to have_key(:attributes)
+    expect(login[:data][:attributes]).to have_key(:api_key)
   end
 end
