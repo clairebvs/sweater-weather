b002e5a7cb86083ac7ac0879be0d415dc09193c9
ClaireBvs <clairembeauvais@gmail.com>
2018-11-06 08:24:35 -0700

    Add #session_params #find_user and use serializer for SessionController [#8]

--- app/controllers/api/v1/session_controller.rb
+++ app/controllers/api/v1/session_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::SessionController < ApplicationController
   def create
-  end 
+    render json: UserSerializer.new(find_user), status: 200
+  end
+
+  private
+  def find_user
+    @find_user ||= User.find_by_email(session_params[:email])
+  end
+
+  def session_params
+    params.permit(:email, :password)
+  end
 end
