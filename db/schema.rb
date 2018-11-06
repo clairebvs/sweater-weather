45f008b1d0759cd7c4879bab98cbf5cc72bc2647
ClaireBvs <clairembeauvais@gmail.com>
2018-11-06 11:00:56 -0700

    Run migration for Favorite [#9]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181105234008) do
+ActiveRecord::Schema.define(version: 20181106175526) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
