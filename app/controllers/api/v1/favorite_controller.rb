c39be6454412416aa8862da902219e59e12a9c7d
ClaireBvs <clairembeauvais@gmail.com>
2018-11-06 10:57:35 -0700

    Add table and model Favorite [#9]

--- app/controllers/api/v1/favorite_controller.rb
+++ app/controllers/api/v1/favorite_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::FavoriteController < ApplicationController
   def create
+
+    render json: favorite, status: 200
   end
 end
---
+++ spec/factories/favorites.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :favorite do
+    references { "" }
+    location { "MyString" }
+  end
+end
