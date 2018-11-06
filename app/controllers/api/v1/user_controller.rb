26c0465ef466a64f6082abde13d6988156b60542
ClaireBvs <clairembeauvais@gmail.com>
2018-11-06 07:15:45 -0700

    add #user to UserController and use serializer [#7]

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::UserController < ApplicationController
   def create
-    user = User.new(user_params)
-    saved_user = user.save
-    render json: saved_user, status: 201
+    render json: UserSerializer.new(user), status: 201
   end
 
   private
+
+  def user
+    @user ||= User.create(user_params)
+  end
 
   def user_params
     params.permit(:email, :password, :password_confirmation)
