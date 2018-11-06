b62f743ed6af22b89660202323b0bc0797aa0e9a
ClaireBvs <clairembeauvais@gmail.com>
2018-11-06 12:47:01 -0700

    Add request test sad path/no api key for api/v1/favorite [#15]

--- spec/requests/favoriting_locations_spec.rb
+++ spec/requests/favoriting_locations_spec.rb
@@ -18,4 +18,16 @@ describe 'POST /api/v1/favorites' do
     expect(favorite[:data]).to have_key(:attributes)
     expect(favorite[:data][:attributes]).to have_key(:location)
   end
+
+  it 'returns 401 (Unauthorized) if no API key' do
+    user = User.create(password: "password", email: "myemail@so.com", api_key: "jgn983hy48thw9begh98h4539h4")
+
+    favorite_params = {
+                        location: "Denver, CO"
+                      }
+
+    post "/api/v1/favorites", params: favorite_params
+
+    expect(response.status).to eq 401
+  end
 end
