e6cb4104bc0b95d9450387778e1713b781499192
ClaireBvs <clairembeauvais@gmail.com>
2018-11-06 12:40:36 -0700

    Add #favorite_params, #user, #add_favorite to FavoriteController [#9]

--- app/controllers/api/v1/favorite_controller.rb
+++ app/controllers/api/v1/favorite_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::FavoriteController < ApplicationController
   def create
+    render json: FavoriteSerializer.new(add_favorite), status: 200
+  end
 
-    render json: favorite, status: 200
+  private
+  def favorite_params
+    params.permit(:api_key, :location)
+  end
+
+  def user
+    User.find_by_api_key(favorite_params[:api_key])
+  end
+
+  def add_favorite
+    user.favorites.find_or_create_by(location: favorite_params[:location])
   end
 end
